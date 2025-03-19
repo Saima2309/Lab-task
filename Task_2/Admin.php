a24fdf8202aed798725636820481d4f263147713
Saima Ahmed <saimaahmed0923@gmail.com>
2025-03-20 02:46:41 +0600

    Version 1

--- Task_2/Admin.php
+++ Task_2/Admin.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Owner Registration Form</title>
+    <link rel="stylesheet" type="text/css" href="mystyle1.css">
 </head>
 <body>
     <div class="form-container">
